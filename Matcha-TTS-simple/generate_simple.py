91865e16742719d3b1dd4409bf8addfc87f553ec
clcarwin <clcarwin@gmail.com>
2023-11-02 00:14:42 +0800

    generate multi-speaker tts ok

--- Matcha-TTS-simple/generate_simple.py
+++ Matcha-TTS-simple/generate_simple.py
@@ -1,3 +1,4 @@
+import os,sys,argparse,json
 import datetime as dt
 from pathlib import Path
 
@@ -17,125 +18,32 @@ from matcha.models.matcha_tts import MatchaTTS
 from matcha.text import sequence_to_text, text_to_sequence
 from matcha.utils.model import denormalize
 from matcha.utils.utils import intersperse
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-MATCHA_CHECKPOINT = "checkpoints/model_00150000.pt"
-HIFIGAN_CHECKPOINT = "checkpoints/hifigan_T2_v1"
-OUTPUT_FOLDER = "logs/simple"
+from matcha.utils.utils import dict_to_attrdic
 
 
 
-
-data_param = {
-    "name": "ljspeech",
-    "train_filelist_path": "data/filelists/ljs_audio_text_train_filelist.txt",
-    "valid_filelist_path": "data/filelists/ljs_audio_text_val_filelist.txt",
-    "batch_size": 32,
-    "num_workers": 20,
-    "pin_memory": True,
-    "cleaners": ["english_cleaners2"],
-    "add_blank": True,
-    "n_spks": 1,
-    "n_fft": 1024,
-    "n_feats": 80,
-    "sample_rate": 22050,
-    "hop_length": 256,
-    "win_length": 1024,
-    "f_min": 0,
-    "f_max": 8000,
-    "data_statistics":  # Computed for ljspeech dataset
-    {"mel_mean": -5.536622,
-    "mel_std": 2.116101},
-    "seed": 42
-}
-
-encoder_param = AttrDict({
-    "encoder_type": "RoPE Encoder",
-    "encoder_params":AttrDict({
-        "n_feats": 80,
-        "n_channels": 192,
-        "filter_channels": 768,
-        "filter_channels_dp": 256,
-        "n_heads": 2,
-        "n_layers": 6,
-        "kernel_size": 3,
-        "p_dropout": 0.1,
-        "spk_emb_dim": 64,
-        "n_spks": 1,
-        "prenet": True
-    }),
-    "duration_predictor_params": AttrDict({
-        "filter_channels_dp": 256,
-        "kernel_size": 3,
-        "p_dropout": 0.1,
-    })
-})
+# def load_model(checkpoint_path):
+#     model = MatchaTTS(**model_param).cuda()
+#     model.load_state_dict(torch.load(checkpoint_path, map_location=device))
+#     # model = MatchaTTS.load_from_checkpoint(checkpoint_path, map_location=device)
+#     model.eval()
+#     return model
+# count_params = lambda x: f"{sum(p.numel() for p in x.parameters()):,}"
 
 
-decoder_param = {
-    "channels": [256, 256],
-    "dropout": 0.05,
-    "attention_head_dim": 64,
-    "n_blocks": 1,
-    "num_mid_blocks": 2,
-    "num_heads": 2,
-    "act_fn": "snakebeta"
-}
+# model = load_model(MATCHA_CHECKPOINT)
+# print(f"Model loaded! Parameter count: {count_params(model)}")
 
-cfm_param = AttrDict({
-    "name": "CFM",
-    "solver": "euler",
-    "sigma_min": 1e-4
-})
+# def load_vocoder(checkpoint_path):
+#     h = AttrDict(v1)
+#     hifigan = HiFiGAN(h).to(device)
+#     hifigan.load_state_dict(torch.load(checkpoint_path, map_location=device)['generator'])
+#     _ = hifigan.eval()
+#     hifigan.remove_weight_norm()
+#     return hifigan
 
-model_param = {
-    "n_vocab":178,
-    "n_spks":1,
-    "spk_emb_dim":64,
-    "n_feats":80,
-    "encoder":encoder_param,
-    "decoder":decoder_param,
-    "cfm":cfm_param,
-    "data_statistics":{"mel_mean": -5.536622,"mel_std": 2.116101},
-    "out_size":None
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def load_model(checkpoint_path):
-    model = MatchaTTS(**model_param).cuda()
-    model.load_state_dict(torch.load(checkpoint_path, map_location=device))
-    # model = MatchaTTS.load_from_checkpoint(checkpoint_path, map_location=device)
-    model.eval()
-    return model
-count_params = lambda x: f"{sum(p.numel() for p in x.parameters()):,}"
-
-
-model = load_model(MATCHA_CHECKPOINT)
-print(f"Model loaded! Parameter count: {count_params(model)}")
-
-def load_vocoder(checkpoint_path):
-    h = AttrDict(v1)
-    hifigan = HiFiGAN(h).to(device)
-    hifigan.load_state_dict(torch.load(checkpoint_path, map_location=device)['generator'])
-    _ = hifigan.eval()
-    hifigan.remove_weight_norm()
-    return hifigan
-
-vocoder = load_vocoder(HIFIGAN_CHECKPOINT)
-denoiser = Denoiser(vocoder, mode='zeros')
+# vocoder = load_vocoder(HIFIGAN_CHECKPOINT)
+# denoiser = Denoiser(vocoder, mode='zeros')
 
 
 @torch.inference_mode()
@@ -152,7 +60,7 @@ def process_text(text: str):
 
 
 @torch.inference_mode()
-def synthesise(text, spks=None):
+def synthesise(text, spks=None, n_timesteps=10, length_scale=1.0, temperature=0.667):
     text_processed = process_text(text)
     start_t = dt.datetime.now()
     output = model.synthesise(
@@ -173,60 +81,107 @@ def to_waveform(mel, vocoder):
     audio = denoiser(audio.squeeze(0), strength=0.00025).cpu().squeeze()
     return audio.cpu().squeeze()
     
-def save_to_folder(filename: str, output: dict, folder: str):
-    folder = Path(folder)
-    folder.mkdir(exist_ok=True, parents=True)
-    np.save(folder / f'{filename}', output['mel'].cpu().numpy())
-    sf.write(folder / f'{filename}.wav', output['waveform'], 22050, 'PCM_24')
+# def save_to_folder(filename: str, output: dict, folder: str):
+#     folder = Path(folder)
+#     folder.mkdir(exist_ok=True, parents=True)
+#     np.save(folder / f'{filename}', output['mel'].cpu().numpy())
+#     sf.write(folder / f'{filename}.wav', output['waveform'], 22050, 'PCM_24')
 
 
 
-texts = [
-    "The Secret Service believed that it was very doubtful that any President would ride regularly in a vehicle with a fixed top, even though transparent.",
-    "hi, how are you? can you help me? i want to go to school."
-]
+# texts = [
+#     "The Secret Service believed that it was very doubtful that any President would ride regularly in a vehicle with a fixed top, even though transparent.",
+#     "hi, how are you? can you help me? i want to go to school."
+# ]
 
-## Number of ODE Solver steps
-n_timesteps = 10
+# ## Number of ODE Solver steps
+# n_timesteps = 10
 
-## Changes to the speaking rate
-length_scale=1.0
+# ## Changes to the speaking rate
+# length_scale=1.0
 
-## Sampling temperature
-temperature = 0.667
+# ## Sampling temperature
+# temperature = 0.667
 
 
-outputs, rtfs = [], []
-rtfs_w = []
-for i, text in enumerate(tqdm(texts)):
-    output = synthesise(text) #, torch.tensor([15], device=device, dtype=torch.long).unsqueeze(0))
+# outputs, rtfs = [], []
+# rtfs_w = []
+# for i, text in enumerate(tqdm(texts)):
+#     output = synthesise(text) #, torch.tensor([15], device=device, dtype=torch.long).unsqueeze(0))
+#     output['waveform'] = to_waveform(output['mel'], vocoder)
+
+#     # Compute Real Time Factor (RTF) with HiFi-GAN
+#     t = (dt.datetime.now() - output['start_t']).total_seconds()
+#     rtf_w = t * 22050 / (output['waveform'].shape[-1])
+
+#     ## Pretty print
+#     print(f"{'*' * 53}")
+#     print(f"Input text - {i}")
+#     print(f"{'-' * 53}")
+#     print(output['x_orig'])
+#     print(f"{'*' * 53}")
+#     print(f"Phonetised text - {i}")
+#     print(f"{'-' * 53}")
+#     print(output['x_phones'])
+#     print(f"{'*' * 53}")
+#     print(f"RTF:\t\t{output['rtf']:.6f}")
+#     print(f"RTF Waveform:\t{rtf_w:.6f}")
+#     rtfs.append(output['rtf'])
+#     rtfs_w.append(rtf_w)
+
+#     ## Display the synthesised waveform
+#     # ipd.display(ipd.Audio(output['waveform'], rate=22050))
+
+#     ## Save the generated waveform
+#     save_to_folder(i, output, OUTPUT_FOLDER)
+
+# print(f"Number of ODE steps: {n_timesteps}")
+# print(f"Mean RTF:\t\t\t\t{np.mean(rtfs):.6f} ± {np.std(rtfs):.6f}")
+# print(f"Mean RTF Waveform (incl. vocoder):\t{np.mean(rtfs_w):.6f} ± {np.std(rtfs_w):.6f}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MatchaTTS')
+    parser.add_argument('--config', required=True, type=str, help='configs/xxx.json')
+    parser.add_argument('--ckpt', required=True, type=str, help='checkpoint of matcha')
+    parser.add_argument('--hifigan', type=str, default='checkpoints/hifigan_T2_v1')
+    parser.add_argument('--spks', type=int, default=None, help='None for ljspeech, 0-109 for vctk')
+
+    parser.add_argument('--text', type=str, default='The Secret Service believed that it was very doubtful that any President would ride regularly in a vehicle with a fixed top, even though transparent.')
+    parser.add_argument('--output', type=str, default='output_tts.wav')
+    args = parser.parse_args()
+    print(args)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    args.output = os.path.abspath(args.output)
+
+    cfg = json.load(open(args.config))
+    cfg = dict_to_attrdic(cfg)
+
+    if cfg.model_param.encoder.encoder_params.n_spks > 1 and args.spks is None:
+        print('ERROR: --spks can not be None when run multi-speaker mode')
+        exit()
+    if cfg.model_param.encoder.encoder_params.n_spks == 1 and not args.spks is None:
+        print('ERROR: --spks must be None when run single-speaker mode')
+        exit()
+    if not args.spks is None:
+        args.spks = torch.tensor([args.spks], dtype=torch.int64)
+        args.spks = args.spks.to(device)
+
+    model = MatchaTTS(**cfg.model_param).to(device)
+    model.load_state_dict(torch.load(args.ckpt, map_location=device))
+    model.eval()
+
+    hifigan = HiFiGAN(dict_to_attrdic(v1)).to(device)
+    hifigan.load_state_dict(torch.load(args.hifigan, map_location=device)['generator'])
+    hifigan.eval()
+    hifigan.remove_weight_norm()
+    vocoder = hifigan
+    denoiser = Denoiser(vocoder, mode='zeros')
+
+    output = synthesise(args.text, spks=args.spks, n_timesteps=10, length_scale=1.0, temperature=0.667)
     output['waveform'] = to_waveform(output['mel'], vocoder)
 
-    # Compute Real Time Factor (RTF) with HiFi-GAN
-    t = (dt.datetime.now() - output['start_t']).total_seconds()
-    rtf_w = t * 22050 / (output['waveform'].shape[-1])
-
-    ## Pretty print
-    print(f"{'*' * 53}")
-    print(f"Input text - {i}")
-    print(f"{'-' * 53}")
-    print(output['x_orig'])
-    print(f"{'*' * 53}")
-    print(f"Phonetised text - {i}")
-    print(f"{'-' * 53}")
-    print(output['x_phones'])
-    print(f"{'*' * 53}")
-    print(f"RTF:\t\t{output['rtf']:.6f}")
-    print(f"RTF Waveform:\t{rtf_w:.6f}")
-    rtfs.append(output['rtf'])
-    rtfs_w.append(rtf_w)
-
-    ## Display the synthesised waveform
-    # ipd.display(ipd.Audio(output['waveform'], rate=22050))
-
-    ## Save the generated waveform
-    save_to_folder(i, output, OUTPUT_FOLDER)
-
-print(f"Number of ODE steps: {n_timesteps}")
-print(f"Mean RTF:\t\t\t\t{np.mean(rtfs):.6f} ± {np.std(rtfs):.6f}")
-print(f"Mean RTF Waveform (incl. vocoder):\t{np.mean(rtfs_w):.6f} ± {np.std(rtfs_w):.6f}")
+    filedir_path = os.path.dirname(args.output)
+    os.makedirs(filedir_path, exist_ok=True)
+    sf.write(args.output, output['waveform'], 22050, 'PCM_24')
+    print(f'output: {args.output}')
